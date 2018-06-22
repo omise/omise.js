1e1fe68db31fcd0237f92487c9a274bce6684a05
Sarunyhot Suvannachoti <sarunyhot@omise.co>
2018-06-22 20:42:08 +0700

    Separate config of webpack.

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const merge = require('webpack-merge')
+const path = require('path')
+const webpack = require('webpack')
+const CleanPlugin = require('clean-webpack-plugin')
+const common = require('./webpack.common.js')
+
+const config = merge(common, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+})
+console.log(config)
+
+module.exports = config
