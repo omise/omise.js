db60ee8a208bfcc0acb48899d500af97c9bab035
Jon Randy <jonrandy@gmail.com>
2020-04-17 16:57:40 +0700

    Remove old files and commit reformatting by prettier

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
  */
 
-import 'helpers/polyfill';
+import 'helpers/polyfill'
 
-import 'vendors/easyXDM.js';
+import 'vendors/easyXDM.js'
 
-import config from './config';
-import OmiseFactory from './Omise';
-import OmiseCardFactory from './OmiseCard';
+import config from './config'
+import OmiseFactory from './Omise'
+import OmiseCardFactory from './OmiseCard'
 
-export default OmiseFactory;
+export default OmiseFactory
 
 /* dev:start */
-console.log('Development mode');
+console.log('Development mode')
 /* dev:end */
 
-global.Omise = OmiseFactory(config);
-global.OmiseCard = OmiseCardFactory(config);
-
+global.Omise = OmiseFactory(config)
+global.OmiseCard = OmiseCardFactory(config)
